bf5ba629677da94e80f191966cd0fd7281f2ebc5
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-06 15:43:56 +0800

    feature: send email on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,45 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const sendgridMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email } = req.body;
   User.findOne({ email }).exec((err, user) => {
     if (user) {
       return res.status(400).json({
         error: "Email is taken"
       });
     }
-  });
 
-  let newUser = new User({ name, email, password });
-  newUser.save((err, success) => {
-    if (err) {
-      console.log("SIGNUP ERROR", err);
-      return res.status(400).json({
-        error: err
+    const token = jwt.sign(
+      { name, email },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "10m" }
+    );
+
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: "Account activation link",
+      html: `<h1>Please use the following link to activate your account</h1>
+        <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+        <hr />
+        <p> This email may contain sensitive information</p>
+        <p>${process.env.CLIENT_URL}</p>`
+    };
+
+    sendgridMail
+      .send(emailData)
+      .then(sent => {
+        return res.json({
+          message: `Email has been sent to ${email}. Follow the instruction to activate your account.`
+        });
+      })
+      .catch(err => {
+        res.json({ message: err.message });
       });
-    }
-    res.json({
-      message: "Sign-up success! Please sign in"
-    });
   });
 };
