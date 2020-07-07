858b622649c1aa9a14c959154c76f662909b90bf
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-07 13:08:20 +0800

    feature: user sign-in

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { signup, accountActivation } = require("../controllers/auth");
-const { userSignupValidator } = require("../validators/auth");
+const { signup, accountActivation, signin } = require("../controllers/auth");
+const {
+  userSignupValidator,
+  userSigninValidator
+} = require("../validators/auth");
 const { runValidation } = require("../validators");
 
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/account-activation", accountActivation);
+router.post("/signin", userSigninValidator, runValidation, signin);
 
 module.exports = router;
