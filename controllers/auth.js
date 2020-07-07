949e87d0a0914527235992b4dc419fa2035d18c7
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-07 09:39:09 +0800

    feature: account activation with sendgrid mail

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.signup = (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, password } = req.body;
   User.findOne({ email }).exec((err, user) => {
     if (user) {
       return res.status(400).json({
@@ -15,7 +15,7 @@ exports.signup = (req, res) => {
     }
 
     const token = jwt.sign(
-      { name, email },
+      { name, email, password },
       process.env.JWT_ACCOUNT_ACTIVATION,
       { expiresIn: "10m" }
     );
@@ -42,4 +42,42 @@ exports.signup = (req, res) => {
         res.json({ message: err.message });
       });
   });
+};
+
+exports.accountActivation = (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(
+      err,
+      decoded
+    ) {
+      if (err) {
+        console.log("JWT VERIFY IN ACCOUNT ACTIVATION ERROR", err);
+        return res.status(401).json({
+          error: "The link has expired. Please re-try."
+        });
+      }
+
+      const { name, email, password } = jwt.decode(token);
+
+      const user = new User({ name, email, password });
+      user.save((err, user) => {
+        if (err) {
+          console.log("SAVE USER IN ACCOUNT ACTIVATION ERROR ", err);
+          return res.status(401).json({
+            error: "Error saving user in database. Please try again."
+          });
+        }
+
+        return res.json({
+          message: "Sign-up success. Please sign-in"
+        });
+      });
+    });
+  } else {
+    return res.json({
+      message: "Something went wrong. Please try again."
+    });
+  }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const {signup} = require('../controllers/auth')
-const {userSignupValidator} = require('../validators/auth')
-const {runValidation} = require('../validators')
+const express = require("express");
+const router = express.Router();
+const { signup, accountActivation } = require("../controllers/auth");
+const { userSignupValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
 
-router.post('/signup', userSignupValidator, runValidation, signup)
+router.post("/signup", userSignupValidator, runValidation, signup);
+router.post("/account-activation", accountActivation);
 
-module.exports = router
+module.exports = router;
